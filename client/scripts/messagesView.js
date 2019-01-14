41ad934b1ce69e9d5f86c36a79887d36108aec3a
Kyruski <jamescboyett@gmail.com>
2019-01-14 12:34:25 -0600

    Implements partial messagesView functionality

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,6 +6,21 @@ var MessagesView = {
   },
 
   render: function() {
-  }
+  },
 
+  renderMessage: function() {
+    $.getJSON('parse.js', function(data) {
+      let html = '';
+      for (let i = 0; i < data.order.length; i++) {
+        html += MessageView.render(data.order[i]);
+      }
+      $chats.append(html);
+    });
+  }
 };
+
+// var i, html = "";
+// for (i = 0; i < data.order.length; i++) {
+//   html += compiled(data.movies[i]);
+// }  
+// $("#movies").append(html);
